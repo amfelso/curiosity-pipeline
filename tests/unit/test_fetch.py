0b7d3debe429a2dffe0eaaa3f6ef3c9e54bea841
August Felso <augustfelso@gmail.com>
2024-12-04 00:38:02 -0500

    Fix unit tests

--- tests/unit/test_fetch.py
+++ tests/unit/test_fetch.py
@@ -1,16 +1,18 @@
 from functions.fetch_mars_images import app
+import json
 
 
 def test_fetch_images():
     input_payload = {"sols": [4061]}
 
     data = app.lambda_handler(input_payload, "")
+    first_photo = json.loads(data["body"])[0]
 
     assert data["statusCode"] == 200
     assert len(data["body"]) > 0
-    assert "img_src" in data["body"][0]
-    assert "earth_date" in data["body"][0]
-    assert "sol" in data["body"][0]
-    assert "camera" in data["body"][0]
-    assert "rover" in data["body"][0]
-    assert "id" in data["body"][0]
+    assert "img_src" in first_photo
+    assert "earth_date" in first_photo
+    assert "sol" in first_photo
+    assert "camera" in first_photo
+    assert "rover" in first_photo
+    assert "id" in first_photo
--- tests/unit/test_generate.py
+++ tests/unit/test_generate.py
@@ -1,6 +1,6 @@
-from functions.generate_image_embeddings import app
+from functions.process_mars_metadata import app
 
 
-def test_stock_checker():
+def test_process_metadata():
     data = app.lambda_handler(None, "")
     assert 0 <= data["stock_price"] >= 0 <= 100
