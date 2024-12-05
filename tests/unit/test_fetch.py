8c027fa21c9d947be539705b382c0e2fe898bc20
August Felso <augustfelso@gmail.com>
2024-12-05 01:56:36 -0500

    Update unit test

--- tests/unit/test_fetch.py
+++ tests/unit/test_fetch.py
@@ -10,9 +10,7 @@ def test_fetch_images():
 
     assert data["statusCode"] == 200
     assert len(data["body"]) > 0
-    assert "img_src" in first_photo
-    assert "earth_date" in first_photo
-    assert "sol" in first_photo
-    assert "camera" in first_photo
-    assert "rover" in first_photo
     assert "id" in first_photo
+    assert "sol" in first_photo
+    assert "earth_date" in first_photo
+    assert "img_src" in first_photo
