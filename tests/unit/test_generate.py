85c29d4045c7325f7b6ae8f36dbfa7e2c94d5ecb
August Felso <augustfelso@gmail.com>
2024-12-07 16:21:59 -0500

    Update test cases to use consistent earth_date value of 2012-08-06

--- tests/unit/test_generate.py
+++ tests/unit/test_generate.py
@@ -4,7 +4,7 @@ import json
 
 def test_generate_memories():
     input_payload = {
-        "earth_date": "2024-01-08",
+        "earth_date": "2012-08-06",
         "fetch_result": {
             'statusCode': 200,
             'body': json.dumps([])
