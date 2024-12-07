34e8962913346ab53d56a95ab9d90f445e6d05e2
August Felso <augustfelso@gmail.com>
2024-12-06 19:51:51 -0500

    Fix unit test input

--- tests/unit/test_embed.py
+++ tests/unit/test_embed.py
@@ -1,12 +1,19 @@
 from functions.embed_memories_to_pinecone import app
 from dotenv import load_dotenv
+import json
 
 # Load environment variables from .env file
 load_dotenv()
 
 
 def test_embed_memories():
-    input_payload = {"urls": []}
+    input_payload = {
+        "earth_date": "2024-01-08",
+        "process_result": {
+            'statusCode': 200,
+            'body': json.dumps([])
+        }
+    }
 
     data = app.lambda_handler(input_payload, "")
 
