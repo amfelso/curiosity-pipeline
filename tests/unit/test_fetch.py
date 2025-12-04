7ad088126f1966c03e9770735fb0d3081566b040
August Felso <august.felso@cloudzero.com>
2025-12-04 01:39:04 -0500

    Fix linting

--- tests/unit/test_fetch.py
+++ tests/unit/test_fetch.py
@@ -11,12 +11,18 @@ def test_fetch_images():
             "id": 2674,
             "sol": 1,
             "camera": {"name": "NAVCAM"},
-            "img_src": "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00001/opgs/edr/ncam/NRA_397586928EDR_F0010008AUT_04096M_.JPG",
+            "img_src": (
+                "http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/"
+                "surface/sol/00001/opgs/edr/ncam/NRA_397586928EDR_F0010008AUT_04096M_.JPG"
+            ),
             "earth_date": "2012-08-07"
         }
     ]
 
-    with patch('functions.fetch_images_with_metadata.app.fetch_images_by_sol', return_value=mock_photos):
+    with patch(
+        'functions.fetch_images_with_metadata.app.fetch_images_by_sol',
+        return_value=mock_photos
+    ):
         data = app.lambda_handler(input_payload, "")
 
     assert data["statusCode"] == 200
